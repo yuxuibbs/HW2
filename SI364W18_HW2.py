d3587c0e033afc91ab3b5a1b85b49b29989bd6c8
jczetta <jczetta@gmail.com>
2018-01-15 14:07:03 -0500

    Clarity and reloader added to code file

--- SI364W18_HW2.py
+++ SI364W18_HW2.py
@@ -2,13 +2,14 @@
 ## Winter 2018
 ## HW 2 - Part 1
 
-## This homework has 2 parts. This file is the basis for HW 3 part 1.
+## This homework has 3 parts, all of which should be completed inside this file.
 
-## Add view functions to this Flask application code below so that the routes described in the README exist and render the templates they are supposed to (all templates provided inside the HW3Part1/templates directory).
+## Add view functions to this Flask application code below so that the routes described in the README exist and render the templates they are supposed to (all templates provided are inside the templates/ directory, where they should stay).
+
+## As part of the homework, you may also need to add templates (new .html files) to the templates directory.
 
 from flask import Flask, request
 app = Flask(__name__)
-app.debug = True
 
 @app.route('/')
 def hello_world():
@@ -19,4 +20,4 @@ def hello_user(name):
     return '<h1>Hello {0}<h1>'.format(name)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(use_reloader=True,debug=True)
