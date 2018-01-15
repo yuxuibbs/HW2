523c0ee234023f8599994e5b1f57c7dee6673de5
jczetta <jczetta@gmail.com>
2018-01-15 16:13:40 -0500

    Resolve conflicts and include correct imports/structure

--- SI364W18_HW2.py
+++ SI364W18_HW2.py
@@ -4,24 +4,53 @@
 
 ## This homework has 3 parts, all of which should be completed inside this file.
 
-## Add view functions to this Flask application code below so that the routes described in the README exist and render the templates they are supposed to (all templates provided are inside the templates/ directory, where they should stay).
+## Add view functions and any other necessary code to this Flask application code below so that the routes described in the README exist and render the templates they are supposed to (all templates provided are inside the templates/ directory, where they should stay).
 
 ## As part of the homework, you may also need to add templates (new .html files) to the templates directory.
 
+#############################
+##### IMPORT STATEMENTS #####
+#############################
+
 from flask import Flask, request, render_template
+<<<<<<< HEAD
 import FlaskForm
 from wtforms import StringField, SubmitField, RadioField, ValidationError
 from wtforms.validators import Required
 
+=======
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, RadioField, ValidationError
+from wtforms.validators import Required
+
+#####################
+##### APP SETUP #####
+#####################
+
+>>>>>>> a6eb0a1... Add structure and import stmts necessary
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hardtoguessstring'
+
+####################
+###### FORMS #######
+####################
+
+
+
+
+####################
+###### ROUTES ######
+####################
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+
 @app.route('/user/<name>')
 def hello_user(name):
     return '<h1>Hello {0}<h1>'.format(name)
+
 
 if __name__ == '__main__':
     app.run(use_reloader=True,debug=True)
